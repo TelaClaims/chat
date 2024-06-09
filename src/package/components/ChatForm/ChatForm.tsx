e99849d0437961beb57de7cd15b75de56baa762c
ArieldRio <arieldelrioviamonte@gmail.com>
2024-06-09 01:33:30 -0400

    chore: Add emoji-picker-react npm dependency

--- src/package/components/ChatForm/ChatForm.tsx
+++ src/package/components/ChatForm/ChatForm.tsx
@@ -1,10 +1,18 @@
-import { Box, IconButton, InputAdornment, TextField } from "@mui/material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import { colors } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useChat, useChatDispatch } from "@/package/context/Chat/context";
 import { MessageSelectedPlaceHolder } from "../MessageSelectedPlaceHolder/MessageSelectedPlaceHolder";
 import { SendMediaButton } from "../SendMediaButton/SendMediaButton";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import EmojiPicker from "emoji-picker-react";
 
 interface Props {
   goToLastMessage: () => void;
@@ -15,6 +23,7 @@ export const ChatForm = ({ goToLastMessage }: Props) => {
   const { selectMessage, setAlert } = useChatDispatch();
   const { conversation } = activeConversation || {};
   const messageInputRef = useRef<HTMLInputElement>(null);
+  const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false);
 
   const handleTyping = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
@@ -27,6 +36,7 @@ export const ChatForm = ({ goToLastMessage }: Props) => {
   const handleSendMessage = async () => {
     const cleanMessage = messageInputRef.current?.value?.trim();
     if (cleanMessage && conversation) {
+      hideEmojiPicker();
       if (selectedMessage) {
         const { message, reason } = selectedMessage;
         if (message && reason === "edit") {
@@ -50,6 +60,7 @@ export const ChatForm = ({ goToLastMessage }: Props) => {
 
   const handleSendMediaMessage = async (file: File) => {
     try {
+      hideEmojiPicker();
       const fileBlob = await file.arrayBuffer();
 
       const sendMediaOptions = {
@@ -73,6 +84,12 @@ export const ChatForm = ({ goToLastMessage }: Props) => {
     }
   };
 
+  const hideEmojiPicker = () => {
+    if (isEmojiPickerOpen) {
+      setIsEmojiPickerOpen(false);
+    }
+  };
+
   useEffect(() => {
     if (selectedMessage) {
       const { message, reason } = selectedMessage;
@@ -93,13 +110,38 @@ export const ChatForm = ({ goToLastMessage }: Props) => {
           onClose={selectMessage}
         />
       )}
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: 55,
+          right: 0,
+          left: 0,
+          maxHeight: isEmojiPickerOpen ? "370px" : "0px",
+          overflow: "hidden",
+          transition: "max-height 0.2s ease",
+          "& .EmojiPickerReact": {
+            "--epr-emoji-size": "1.3rem",
+          },
+        }}
+      >
+        <EmojiPicker
+          onEmojiClick={(emojiObject) => {
+            messageInputRef.current!.value += emojiObject.emoji;
+          }}
+          skinTonesDisabled
+          lazyLoadEmojis
+          height={"370px"}
+          width="100%"
+          previewConfig={{ showPreview: false }}
+        />
+      </Box>
       <Box display={"flex"}>
         <SendMediaButton onSelectedFile={handleSendMediaMessage} />
         <TextField
           sx={{
             zIndex: 2,
             bgcolor: colors.grey["100"],
-
+            pr: 0,
             "& .MuiInputLabel-shrink": {
               transform: "translate(14px, -3px) scale(0.75)",
             },
@@ -113,11 +155,28 @@ export const ChatForm = ({ goToLastMessage }: Props) => {
           InputLabelProps={{ shrink: true }}
           InputProps={{
             onKeyDown: handleTyping,
+            sx: {
+              pr: 0,
+            },
             endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={handleSendMessage}>
-                  <SendIcon />
-                </IconButton>
+              <InputAdornment position="end" sx={{ ml: "auto" }}>
+                <Box display={"flex"} alignItems={"center"}>
+                  <IconButton
+                    color={isEmojiPickerOpen ? "primary" : "default"}
+                    onClick={() => setIsEmojiPickerOpen(!isEmojiPickerOpen)}
+                  >
+                    <InsertEmoticonIcon />
+                  </IconButton>
+                  <Divider
+                    orientation="vertical"
+                    flexItem
+                    variant="middle"
+                    sx={{ mx: 1 }}
+                  />
+                  <IconButton onClick={handleSendMessage}>
+                    <SendIcon />
+                  </IconButton>
+                </Box>
               </InputAdornment>
             ),
           }}
